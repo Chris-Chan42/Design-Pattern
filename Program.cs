8ddf44044d560d7eccaa417564f799764f035d9c
christian garcia <christian.garcia@student.centralia.edu>
2025-11-09 20:46:23 -0800

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Design_Pattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+
+
+}
